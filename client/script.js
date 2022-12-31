dc20be5e1a956a9877863189e003c2a799c41676
tuner11 <thulanicronk@Thulanis-MacBook-Pro.local>
2022-12-30 19:58:45 -0600

    changed host to render instead of localhost

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://tooly-yo58.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
